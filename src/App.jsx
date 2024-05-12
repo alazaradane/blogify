9ca8f0f328fc01ec56ddbad0128ab985924d8f36
alazaradane <alazargithub369@gmail.com>
2024-05-12 19:15:04 +0300

    adding signin and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 
-import {createBrowserRouter, Route, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Write from './pages/Write';
+import Single from './pages/Single'
+import PageNotFound from './pages/PageNotFound'
+
+const Layout = () =>{
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Layout/>,
+    children:[
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/write',
+        element: <Write/>
+      },
+      {
+        path:'/post/:id',
+        element: <Single/>,
+        errorElement: <PageNotFound/>
+      }
+    ],
+    errorElement: <PageNotFound/>
+    
   },
   {
     path: "/register",
@@ -20,6 +52,9 @@ const router = createBrowserRouter([
   },
 ]);
 
+
+
+
 const App = () => {
   return (
     <div>
@@ -27,5 +62,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App
