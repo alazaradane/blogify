b6796a5a940bc711cc876c403b7e828db014e9ff
alazaradane <alazargithub369@gmail.com>
2024-05-13 21:01:55 +0300

    adding post objects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+  const posts  = [
+    {
+      id:1,
+      title: 'Lorem ipsum dolor sit amet',
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus saepe aspernatur praesentium, ex in molestias minus eos quas sapiente similique.",
+      img: 'https://images.unsplash.com/photo-1533488069324-f9265c15d37f?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      id:2,
+      title: 'Lorem ipsum dolor sit amet',
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus saepe aspernatur praesentium, ex in molestias minus eos quas sapiente similique.",
+      img: 'https://images.unsplash.com/photo-1635070041078-e363dbe005cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      id:3,
+      title: 'Lorem ipsum dolor sit amet',
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus saepe aspernatur praesentium, ex in molestias minus eos quas sapiente similique.",
+      img: 'https://images.unsplash.com/photo-1519238359922-989348752efb?q=80&w=1669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      id:4,
+      title: 'Lorem ipsum dolor sit amet',
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus saepe aspernatur praesentium, ex in molestias minus eos quas sapiente similique.",
+      img: 'https://images.unsplash.com/photo-1609220136736-443140cffec6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5fGVufDB8fDB8fHww'
+    },
+  ]
+
   return (
-    <div>Home</div>
+    <div className=' container'>
+      <div>
+        {posts.map((post)=>(
+          <div key={post.id} className=' flex'>
+            <div>
+              <img src={post.img} alt="" className=' w-[10rem]' />
+            </div>
+            <div>
+              <Link to={`/post/${post.id}`}>
+                <p>{post.title}</p>
+              </Link>
+              <p>{post.desc}</p>
+              <button>Read More...</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
