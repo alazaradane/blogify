b870895a8de26c79fb469fdeeb83fc7380afed2b
alazaradane <alazargithub369@gmail.com>
2024-05-14 06:31:29 +0300

    adding react quill in writing component

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 const Write = () => {
+
+  const [value, setValue] = useState('');
+  console.log(value)
+
   return (
-    <div>Write</div>
+    <section className=' w-full flex items-start my-[5rem]  px-[10rem]'>
+      <div className=' flex flex-col gap-[5rem] w-2/3'>
+        <input type="text" placeholder='Title...' className=' border-[1.5px] border-slate-800 py-1 px-1 focus:outline-none placeholder:ml-[1rem] focus:border-indigo-700 '/>
+        <div>
+          <ReactQuill theme="snow" value={value} onChange={setValue} />;
+        </div>
+      </div>
+      <div className=' w-1/3 mx-auto flex flex-col justify-center items-center gap-[1rem]'>
+        <div>item1</div>
+        <div>item2</div>
+      </div>
+    </section>
   )
 }
 
